0d3a7e7bfaf506a149fad9bef065a2f3a9133cd6
ahnyoungkeun <tosan2094@gmail.com>
2020-09-29 12:58:40 +0900

    워커신청 유효성 처리중

--- AniWalk/src/main/webapp/WEB-INF/static/js/main.js
+++ AniWalk/src/main/webapp/WEB-INF/static/js/main.js
@@ -1,6 +1,19 @@
-let agreeError = '필수 항목에 동의하셔야 합니다.';
-let profileImgError = '사진 1개이상 업로드 하셔야합니다.'
-let twoTextInputError = '2글자 이상 입력하세요';
+const agreeError = '필수 항목에 동의하셔야 합니다.';
+const profileImgError = '사진 1개이상 업로드 하셔야합니다.'
+const twoTextInputError = '2글자 이상 입력하세요';
+const phoneMinusError = `
+        <div class="text-danger">-을 빼고 작성해주세요</div>
+    `;
+const phoneInputError = `
+        <div class="text-danger">핸드폰번호를 제대로 입력해주세요</div>
+    `;
+
+const check_num = /[0-9]/;                  // 숫자
+const check_eng = /[a-zA-Z]/;               // 문자
+const check_spc = /[~!@#$%^&*()_+|<>?:{}]/; // 특수문자
+const check_kor = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;      // 한글체크
+
+
 applyCheck =  function(){
     if(applyForm1.essentialAgreeYn.value === ''){
         alert(agreeError);
@@ -16,14 +29,34 @@ applyCheck =  function(){
 
 
 addPhoneAuthForm = function(){
-    const authPhoneLi = document.querySelector('.auth-phone');
+    const authPart = document.querySelector('.auth-part');
     const addForm = `
-        <form id='authForm' style="display: flex; justify-content: space-around; margin-top: 10px;">
+        <form style="display: flex; justify-content: space-around; margin-top: 10px;">
             <label>인증번호</label>
-            <input class='auth' type="text" class="from-control" style="width: 65%;">
-            <button onclick='auth()' id='authNum' type="button" class="btn btn-primary">인증번호입력</button>
+            <input type="text" class="from-control" style="width: 65%;">
+            <button type="button" class="btn btn-primary">인증번호입력</button>
         </form>
-    `
-    authPhoneLi.innerHTML += addForm;
-}
+    `;
 
+    const inputPhoneNum = document.getElementById('inputPhoneNum');
+    let phoneNum = inputPhoneNum.value.split('');
+    let minusError = 0;
+    for(let i=0; i<phoneNum.length; i++){
+        if(phoneNum[i] === '-'){
+            minusError++;
+        }
+    }
+
+    if(minusError >= 1){
+        authPart.innerHTML = phoneMinusError;
+    }else if (inputPhoneNum.value.length < 11){
+        authPart.innerHTML = phoneInputError;
+    }else if (check_kor.test(inputPhoneNum.value) || check_eng.test(inputPhoneNum.value) || check_spc.test(inputPhoneNum.value) ){
+        authPart.innerHTML = phoneInputError;
+    }
+    else{
+        authPart.innerHTML = addForm;
+    }
+
+
+}
